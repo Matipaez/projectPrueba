1709d12d1fcae9fbb066edffa6c0e80d779d69a0
Nahuel paez <nahuelpaez86@gmail.com>
2022-09-02 10:47:46 -0300

    mi primer commit

---
+++ PlaylistCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistCell.swift
+//  MatiPractica1project
+//
+//  Created by Nahuel Paez on 31/08/2022.
+//
+
+import UIKit
+
+class PlaylistCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
