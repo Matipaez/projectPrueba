7766d9d669a6ed8606ebe4ede99ef2a7bc576f6d
Nahuel paez <matiaspaez@gmail.com>
2022-09-05 21:58:11 -0300

    actualizacion5/sep

--- ChannelCell.swift
+++ ChannelCell.swift
@@ -3,12 +3,15 @@
 //  MatiPractica1project
 //
 //  Created by Nahuel Paez on 31/08/2022.
-//
-
+//let icon  = UIImage(named: "airplayvideo.circle.fill") compartir
+// let icon = UIImage(named: "bell") campana
+// let icon = UIImage(named: "ellipsis.circle.fill") 3 puntitos config
+// let icon = UIImage(named: "magnifyingglass.circle.fill") lupa buscador
 import UIKit
 
 class ChannelCell: UITableViewCell {
-
+    // let icon  = UIImage(named: "square.and.arrow.up")
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
